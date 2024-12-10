220aee2cd46a388677723c0a6be54d8c57f79a3c
Jiho Ro <jihoro@Mac.attlocal.net>
2024-12-09 19:00:34 -0600

    upto ch9

---
+++ go.mod
@@ -0,0 +1,10 @@
+module greenlight.jihoro.com
+
+go 1.23.1
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.9
+)
+
+require golang.org/x/time v0.7.0 // indirect
---
+++ internal/data/runtime.go
@@ -0,0 +1,40 @@
+package data
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
+
+type Runtime int32
+
+func (r Runtime) MarshalJSON() ([]byte, error) {
+	jsonValue := fmt.Sprintf("%d mins", r)
+
+	quotedJsonValue := strconv.Quote(jsonValue)
+
+	return []byte(quotedJsonValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(b []byte) error {
+	unquoted, err := strconv.Unquote(string(b))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquoted, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+	return nil
+}
